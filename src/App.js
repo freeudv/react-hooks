3b19695aa298a608f2c728b99b2004c3c34b219b
Dmitry Umbras <Umbras.D.Vi@sberbank.ru>
2019-06-30 16:03:43 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import './App.css';
+import {useForm} from './useForm'
 
 const App = () => {
-    return <div>Hey</div>;
+    const [values, handleChange] = useForm({email: '', password: ''});
+
+    return (
+        <form>
+            <input
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+            />
+            <input
+                name="password"
+                type="password"
+                value={values.password}
+                onChange={handleChange}
+            />
+        </form>
+    );
 };
 
 export default App;
---
+++ src/useForm.js
@@ -0,0 +1,13 @@
+import {useState} from 'react';
+
+export const useForm = initialState => {
+    const [values, setValues] = useState(initialState);
+
+    const handleChange = e =>
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+
+    return [values, handleChange];
+};
